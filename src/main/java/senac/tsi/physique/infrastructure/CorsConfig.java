71ff8a320ca649e28e11fd3f35488f9788546dbc
yukioarthur <141744810+yukioarthur@users.noreply.github.com>
2026-05-26 17:57:53 -0300

    v3

--- src/main/java/senac/tsi/physique/infrastructure/CorsConfig.java
+++ src/main/java/senac/tsi/physique/infrastructure/CorsConfig.java
@@ -22,7 +22,8 @@ public class CorsConfig implements WebMvcConfigurer {
             "http://127.0.0.1:3000",
             "http://127.0.0.1:5173",
             "http://127.0.0.1:4200",
-            "https://yukioarthur.github.io"
+            "https://yukioarthur.github.io",
+            "https://physiquewebservice.onrender.com"
     );
 
     private static final List<String> ALLOWED_METHODS = List.of(
